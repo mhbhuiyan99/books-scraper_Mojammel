2bd63b4be3e578e03cda19395ed6f5d0e73ed698
Mojammel Haque Bhuiyan <mhbhuiyan10023@gmail.com>
2026-06-25 15:51:10 +0600

    feat: extract from category(sidebar), get all book containers

--- books_scraper/spiders/books_spider.py
+++ books_scraper/spiders/books_spider.py
@@ -8,5 +8,34 @@ class BooksSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/index.html"]
 
     # Parse method: called for every downloaded page
+    # Website returns HTML, response contains the page
     def parse(self, response):
-        pass
+        # --- CATEGORY EXTRACTION (Homepage) ---
+
+        # Get all category links from sidebar
+        category_links = response.css("div.side_categories ul.nav-list li a::attr(href)").getall()
+        
+        # Get category names
+        category_names = response.css("div.side_categories ul.nav-list a::text").getall()
+
+
+        # --- BOOK EXTRACTION (Category page) ---
+
+        # Get all book containers
+        books = response.css("article.product_pod")
+
+        for book in books:
+            # Extract title from <a>
+            title = book.css("h3 a::attr(title)").get()
+
+            # Extract price
+            price = book.css("p.price_color::text").get()
+
+            # Extract availability
+            avilabilty = book.css("p.availability::text").get()
+
+            yield {
+                "title": title,
+                "price": price,
+                "availability": availability,
+            }
