cb4cab16eac85a832ef8bf59bbf6b607af9da7d3
Mojammel <mhbhuiyan10023@gmail.com>
2026-06-27 11:29:37 +0600

    feat: add custom Item Loader for BookItem with automatic data cleaning

--- books_scraper/items.py
+++ books_scraper/items.py
@@ -1,4 +1,21 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+
+# Input Processors
+def strip_whitespace(value):
+    """Remove leading/trailing whitespace"""
+    return value.strip() if value else value
+
+def remove_currency(value):
+    """Remove £ symbol from price"""
+    return value.replace("£","") if value else value
+
+def normalize_availability(value):
+    """Convert availability text to boolean"""
+    if value and "in stock" in value.lower():
+        return True
+    return False
 
 # Define Item
 class BookItem(scrapy.Item):
@@ -11,4 +28,18 @@ class BookItem(scrapy.Item):
     product_url = scrapy.Field()
     image_url = scrapy.Field()
     category = scrapy.Field()
+
+# Item Loader
+class BookItemLoader(ItemLoader):
+    """
+    Custom loader for BookItem with automatic data cleaning
+    """
+    # Default output: take the first non-null value
+    default_output_processor = TakeFirst()
+
+    # Field-spacific input processors
+    title_in = MapCompose(strip_whitespace)
+    price_in = MapCompose(strip_whitespace, remove_currency)
+    availability_in = MapCompose(strip_whitespace, normalize_availability)
+    category_in = MapCompose(strip_whitespace)
 
